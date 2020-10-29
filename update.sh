09f8d31f1bc5a26a388b354381a3dee51d8d9aa5
machinexa <machinexa@gmail.com>
2020-10-29 14:07:42 +0545

    02:07:42 PM 10/29/2020

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ source /root/.bash_aliases
 current="`pwd`""/"
 cat .gitignore | while read repository
 do
-	cd $repository && gitted # && cd lib && cp "$current""Skipper.py" "Skipper.py"
+	cd $repository && gitted #  && cd lib && cp "$current""PathFunctions.py" "PathFunctions.py"
 	cd $current
 done
